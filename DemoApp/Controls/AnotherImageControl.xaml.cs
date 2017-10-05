2bdb22247f7dc38b210af1d7346ba4fc93672e41
Peter Vietense <peter.vietense@hotmail.de>
2017-10-05 22:37:20 +0200

    added a simple image usercontrol and a nullvalueconverter, so that we dont throw exceptions, when bindings are a little off, but still working

---
+++ DemoApp/Controls/AnotherImageControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace DemoApp
+{
+    public partial class AnotherImageControl
+    {
+        public AnotherImageControl()
+        {
+            InitializeComponent();
+        }
+
+        public string ImagePath
+        {
+            get { return (string)GetValue(ImagePathProperty); }
+            set { SetValue(ImagePathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImagePath.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImagePathProperty =
+            DependencyProperty.Register(nameof(ImagePath), typeof(string), typeof(AnotherImageControl), new PropertyMetadata(string.Empty));
+    }
+}
