d8253c808268c3f2e8ad558d2390f1a85e568d31
Peter Vietense <vietense@itb-pim.de>
2018-12-01 09:29:11 +0100

    added dispatcher abstraction

---
+++ MvvmScarletToolkit/Observables/IScarletDispatcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MvvmScarletToolkit
+{
+    public interface IScarletDispatcher
+    {
+        Task Invoke(Action action);
+        Task<T> Invoke<T>(Func<T> action);
+    }
+}
---
+++ MvvmScarletToolkit/Observables/ScarletDispatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace MvvmScarletToolkit
+{
+    public sealed class ScarletDispatcher : IScarletDispatcher
+    {
+        private readonly Dispatcher _dispatcherObject;
+
+        public ScarletDispatcher()
+        {
+            _dispatcherObject = System.Windows.Application.Current.Dispatcher;
+        }
+
+        public async Task Invoke(Action action)
+        {
+            await _dispatcherObject.InvokeAsync(action);
+        }
+
+        public async Task<T> Invoke<T>(Func<T> action)
+        {
+            return await _dispatcherObject.InvokeAsync<T>(action);
+        }
+    }
+}
