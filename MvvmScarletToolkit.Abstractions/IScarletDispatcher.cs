7f9ad672761a0fbce2ba6d8bda01ee62fe184459
Peter Vietense <peter.vietense@hotmail.de>
2019-06-23 13:58:35 +0200

    refactoring

--- MvvmScarletToolkit.Abstractions/IScarletDispatcher.cs
+++ MvvmScarletToolkit.Abstractions/IScarletDispatcher.cs
@@ -10,17 +10,4 @@ namespace MvvmScarletToolkit.Abstractions
 
         Task<T> Invoke<T>(Func<T> action, CancellationToken token);
     }
-
-    public static class DispatcherExtensions
-    {
-        public static Task Invoke(this IScarletDispatcher dispatcher, Action action)
-        {
-            return dispatcher.Invoke(action, CancellationToken.None);
-        }
-
-        public static Task<T> Invoke<T>(this IScarletDispatcher dispatcher, Func<T> action)
-        {
-            return dispatcher.Invoke(action, CancellationToken.None);
-        }
-    }
 }
