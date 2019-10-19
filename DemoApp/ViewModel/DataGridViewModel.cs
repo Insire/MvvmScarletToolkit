85d115c199e1c3c692bc63b6a913eea34c8c9598
Peter Vietense <peter.vietense@hotmail.de>
2019-10-19 10:05:05 +0200

    binding to a hex string representing a color

--- DemoApp/ViewModel/DataGridViewModel.cs
+++ DemoApp/ViewModel/DataGridViewModel.cs
@@ -21,44 +21,9 @@ namespace DemoApp
                 {
                     CreatedOn = DateTime.Now,
                     Name = Guid.NewGuid().ToString(),
+                    Color = $"#cccc{i.ToString().PadLeft(2, '0')}",
                 });
             }
-        }
-    }
-
-    public class DataGridRowViewModel : ViewModelBase
-    {
-        private string _name;
-        public string Name
-        {
-            get { return _name; }
-            set { SetValue(ref _name, value, OnChanged: () => UpdatedOn = DateTime.Now); }
-        }
-
-        private DateTime _createdOn;
-        public DateTime CreatedOn
-        {
-            get { return _createdOn; }
-            set { SetValue(ref _createdOn, value, OnChanged: () => UpdatedOn = DateTime.Now); }
-        }
-
-        private DateTime _updatedOn;
-        public DateTime UpdatedOn
-        {
-            get { return _createdOn; }
-            private set { SetValue(ref _updatedOn, value); }
-        }
-
-        private bool _isSelected;
-        public bool IsSelected
-        {
-            get { return _isSelected; }
-            set { SetValue(ref _isSelected, value); }
-        }
-
-        public DataGridRowViewModel(ICommandBuilder commandBuilder)
-            : base(commandBuilder)
-        {
         }
     }
 }
