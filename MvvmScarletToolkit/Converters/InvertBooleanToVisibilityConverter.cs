f87422eadb7ef53d518a05a6b80dd16f4c6e3b23
Peter Vietense <peter.vietense@infact-wws.de>
2019-12-16 17:19:17 +0100

    added InvertBooleanToVisibilityConverter

---
+++ MvvmScarletToolkit/Converters/InvertBooleanToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MvvmScarletToolkit
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public sealed class InvertBooleanToVisibilityConverter : ConverterMarkupExtension<InvertBooleanToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool toggle)
+            {
+                if (toggle)
+                    return Visibility.Hidden;
+                else
+                    return Visibility.Visible;
+            }
+
+            return Binding.DoNothing;
+        }
+    }
+}
