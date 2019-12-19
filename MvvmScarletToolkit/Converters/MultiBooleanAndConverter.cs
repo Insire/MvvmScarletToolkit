1a657df3c2332d2017c6fc9817e407c55ed0062f
Peter Vietense <peter.vietense@infact-wws.de>
2019-12-19 13:13:07 +0100

    added MultiBooleanAndConverter

---
+++ MvvmScarletToolkit/Converters/MultiBooleanAndConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MvvmScarletToolkit
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public sealed class MultiBooleanAndConverter : MultiConverterMarkupExtension<MultiBooleanAndConverter>
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = true;
+            for (var i = 0; i < values.Length; i++)
+            {
+                if (values[i] is bool newValue)
+                {
+                    result &= newValue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return result;
+        }
+    }
+}
