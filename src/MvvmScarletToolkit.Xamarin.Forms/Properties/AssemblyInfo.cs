b42a84650787fe007098f4abc04a6b471c0763b0
Peter Vietense <peter.vietense@hotmail.de>
2020-04-13 12:06:40 +0200

    added missing AssemblyInfo.cs to MvvmScarletToolkit.Xamarin.Forms

---
+++ src/MvvmScarletToolkit.Xamarin.Forms/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvvmScarletToolkit.Xamarin.Forms")]
+[assembly: AssemblyDescription("MvvmScarletToolkit.Xamarin.Forms is part of the MvvmScarletToolkit framework, containing concrete implementations for Xamarin.Forms that have been abstracted away in the MvvmScarletToolkit.Abstractions library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9bd9b759-8c12-4e08-971b-9ba21d8247ff")]
