a1973fcc0404ffb5020c18951655551ffb282e3c
Peter Vietense <peter.vietense@hotmail.de>
2020-06-28 09:58:58 +0200

    added example of using a BindingProxy with an ElementName binding

--- src/MvvmScarletToolkit.Samples/MainWindow.xaml.cs
+++ src/MvvmScarletToolkit.Samples/MainWindow.xaml.cs
@@ -1,14 +1,29 @@
 using Jot;
+using System.Windows;
 
 namespace MvvmScarletToolkit.Samples
 {
     public partial class MainWindow
     {
+        public Tracker Tracker
+        {
+            get { return (Tracker)GetValue(TrackerProperty); }
+            set { SetValue(TrackerProperty, value); }
+        }
+
+        public static readonly DependencyProperty TrackerProperty = DependencyProperty.Register(
+            nameof(Tracker),
+            typeof(Tracker),
+            typeof(MainWindow),
+            new PropertyMetadata(null));
+
         public MainWindow(Tracker tracker)
         {
             InitializeComponent();
 
             tracker.Track(this);
+
+            SetCurrentValue(TrackerProperty, tracker);
         }
     }
 }
