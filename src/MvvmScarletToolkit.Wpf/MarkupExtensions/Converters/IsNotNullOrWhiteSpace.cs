ed48dcc189eef61038408e5236906009d0608720
Peter Vietense <peter.vietense@hotmail.de>
2021-03-20 07:08:20 +0100

    added IsNotNullOrWhiteSpace wpf converter markupextension

---
+++ src/MvvmScarletToolkit.Wpf/MarkupExtensions/Converters/IsNotNullOrWhiteSpace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MvvmScarletToolkit
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    public sealed class IsNotNullOrWhiteSpace : ConverterMarkupExtension<IsNullOrEmpty>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string text)
+            {
+                return !string.IsNullOrWhiteSpace(text);
+            }
+
+            return false;
+        }
+    }
+}
