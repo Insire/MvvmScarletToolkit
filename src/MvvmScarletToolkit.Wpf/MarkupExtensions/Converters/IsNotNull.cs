b79daf23906fb8c3c73cde0ee3db5722fcfa1a40
Peter Vietense <peter.vietense@hotmail.de>
2021-03-20 07:15:32 +0100

    added IsNotNull wpf converter markupextension

---
+++ src/MvvmScarletToolkit.Wpf/MarkupExtensions/Converters/IsNotNull.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MvvmScarletToolkit
+{
+    [ValueConversion(typeof(bool), typeof(object))]
+    public sealed class IsNotNull : ConverterMarkupExtension<IsNullOrEmpty>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value is null);
+        }
+    }
+}
