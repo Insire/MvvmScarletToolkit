73955b9fe7e37b66d124f2b20a096a75e0115ed7
Peter Vietense <peter.vietense@hotmail.de>
2021-03-20 07:37:44 +0100

    added extension for ObservableCollection<T> to do an in place sort

---
+++ src/MvvmScarletToolkit/Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace MvvmScarletToolkit.Extensions
+{
+    public static class ObservableCollectionExtensions
+    {
+        /// <summary>
+        /// in place sort an ObservableCollection<typeparamref name="T"/>
+        /// </summary>
+        /// <remarks>
+        /// will undo selection, when collection is bound  for example to a ListBox
+        /// </remarks>
+        public static void Sort<T>(this ObservableCollection<T> collection)
+            where T : IComparable
+        {
+            var sorted = collection.OrderBy(x => x).ToList();
+
+            for (var i = 0; i < sorted.Count; i++)
+            {
+                collection.Move(collection.IndexOf(sorted[i]), i);
+            }
+        }
+    }
+}
