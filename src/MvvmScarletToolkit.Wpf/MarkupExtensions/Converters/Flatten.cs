ee35f5a13995ca55cb512988fbf00ce9f3e03390
Peter Vietense <peter.vietense@hotmail.de>
2021-04-04 14:48:05 +0200

    added Flatten markup extension, which turns an Enumerable of strings into a single string

---
+++ src/MvvmScarletToolkit.Wpf/MarkupExtensions/Converters/Flatten.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MvvmScarletToolkit
+{
+    /// <summary>
+    /// turns an Enumerable of strings into a single string
+    /// </summary>
+    /// <remarks>
+    /// <c>xmlns:mvvm="http://SoftThorn.MvvmScarletToolkit.com/winfx/xaml/shared"</c>
+    /// </remarks>
+    [ValueConversion(typeof(string[]), typeof(string))]
+    public sealed class Flatten : ConverterMarkupExtension<Flatten>
+    {
+        [ConstructorArgument("separator")]
+        public string Separator { get; set; } = ", ";
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IEnumerable<string> strings)
+            {
+                return string.Join(Separator, strings.Where(s => !string.IsNullOrWhiteSpace(s)));
+            }
+
+            return Binding.DoNothing;
+        }
+    }
+}
