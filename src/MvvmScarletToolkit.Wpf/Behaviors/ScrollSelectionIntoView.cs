3e233eca300a986dfe01e3d43d4ace5e08114749
Peter Vietense <peter.vietense@hotmail.de>
2021-04-10 12:11:49 +0200

    [BugFix] ScrollSelectionIntoView only works on ListBox - it now works on DataGrid aswell

--- src/MvvmScarletToolkit.Wpf/Behaviors/ScrollSelectionIntoView.cs
+++ src/MvvmScarletToolkit.Wpf/Behaviors/ScrollSelectionIntoView.cs
@@ -4,6 +4,20 @@ using System.Windows.Controls.Primitives;
 
 namespace MvvmScarletToolkit.Wpf
 {
+    /// <summary>
+    /// Behavior that enables scrolling a SelectedItem of a <see cref="ListBox"/> or <see cref="DataGrid"/> into the View
+    /// </summary>
+    /// <remarks>
+    /// required namespaces:
+    /// <list type="bullet">
+    /// <item>
+    /// <description>xmlns:i="http://schemas.microsoft.com/xaml/behaviors"</description>
+    /// </item>
+    /// <item>
+    /// <description>xmlns:mvvm="http://SoftThorn.MvvmScarletToolkit.com/winfx/xaml/shared"</description>
+    /// </item>
+    /// </list>
+    /// </remarks>
     public sealed class ScrollSelectionIntoView : Behavior<Selector>
     {
         protected override void OnAttached()
@@ -16,6 +30,26 @@ namespace MvvmScarletToolkit.Wpf
 
         private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (sender is DataGrid dataGrid)
+            {
+                if (dataGrid.SelectedItem is null)
+                {
+                    return;
+                }
+
+                var selectedItem = dataGrid.SelectedItem;
+
+                dataGrid.Dispatcher.Invoke(ScrollIntoView);
+                return;
+
+                void ScrollIntoView()
+                {
+                    dataGrid.UpdateLayout();
+
+                    dataGrid.ScrollIntoView(selectedItem);
+                }
+            }
+
             if (sender is ListBox listBox)
             {
                 if (listBox.SelectedItem is null)
@@ -23,19 +57,16 @@ namespace MvvmScarletToolkit.Wpf
                     return;
                 }
 
+                var selectedItem = listBox.SelectedItem;
+
                 listBox.Dispatcher.Invoke(ScrollIntoView);
-            }
 
-            void ScrollIntoView()
-            {
-                listBox.UpdateLayout();
-
-                if (listBox.SelectedItem == null)
+                void ScrollIntoView()
                 {
-                    return;
-                }
+                    listBox.UpdateLayout();
 
-                listBox.ScrollIntoView(listBox.SelectedItem);
+                    listBox.ScrollIntoView(selectedItem);
+                }
             }
         }
 
