69cf7b044fffcf0ee8830785a49b8e2f54073d24
Peter Vietense <peter.vietense@hotmail.de>
2021-08-28 16:10:07 +0200

    added overload to ObservableCollectionExtensions for inplace sorting observablecollections

--- src/MvvmScarletToolkit.Observables/Extensions/ObservableCollectionExtensions.cs
+++ src/MvvmScarletToolkit.Observables/Extensions/ObservableCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using DynamicData.Binding;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MvvmScarletToolkit
@@ -7,15 +8,31 @@ namespace MvvmScarletToolkit
     public static class ObservableCollectionExtensions
     {
         /// <summary>
-        /// in place sort an IObservableCollection<typeparamref name="T"/>
+        /// in place sort an <see cref="IObservableCollection{T}"/>
         /// </summary>
         /// <remarks>
-        /// will undo selection, when collection is bound  for example to a ListBox
+        /// will undo selection, when collection is bound for example to a ListBox
         /// </remarks>
         public static void Sort<T>(this IObservableCollection<T> collection)
             where T : IComparable
         {
             var sorted = collection.OrderBy(x => x).ToList();
+
+            for (var i = 0; i < sorted.Count; i++)
+            {
+                collection.Move(collection.IndexOf(sorted[i]), i);
+            }
+        }
+
+        /// <summary>
+        /// in place sort an <see cref="IObservableCollection{T}"/> utilizing an <see cref="IComparer{T}"/>
+        /// </summary>
+        /// <remarks>
+        /// will undo selection, when collection is bound for example to a ListBox
+        /// </remarks>
+        public static void Sort<T>(this IObservableCollection<T> collection, IComparer<T> comparer)
+        {
+            var sorted = collection.OrderBy(x => x, comparer).ToList();
 
             for (var i = 0; i < sorted.Count; i++)
             {
--- src/MvvmScarletToolkit/Extensions/ObservableCollectionExtensions.cs
+++ src/MvvmScarletToolkit/Extensions/ObservableCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
@@ -7,15 +8,31 @@ namespace MvvmScarletToolkit
     public static class ObservableCollectionExtensions
     {
         /// <summary>
-        /// in place sort an ObservableCollection<typeparamref name="T"/>
+        /// in place sort an <see cref="ObservableCollection{T}"/>
         /// </summary>
         /// <remarks>
-        /// will undo selection, when collection is bound  for example to a ListBox
+        /// will undo selection, when collection is bound for example to a ListBox
         /// </remarks>
         public static void Sort<T>(this ObservableCollection<T> collection)
             where T : IComparable
         {
             var sorted = collection.OrderBy(x => x).ToList();
+
+            for (var i = 0; i < sorted.Count; i++)
+            {
+                collection.Move(collection.IndexOf(sorted[i]), i);
+            }
+        }
+
+        /// <summary>
+        /// in place sort an <see cref="ObservableCollection{T}"/> utilizing an <see cref="IComparer{T}"/>
+        /// </summary>
+        /// <remarks>
+        /// will undo selection, when collection is bound for example to a ListBox
+        /// </remarks>
+        public static void Sort<T>(this ObservableCollection<T> collection, IComparer<T> comparer)
+        {
+            var sorted = collection.OrderBy(x => x, comparer).ToList();
 
             for (var i = 0; i < sorted.Count; i++)
             {
