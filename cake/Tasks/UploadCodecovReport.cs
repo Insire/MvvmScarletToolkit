a953d4ec7e29fd7d02f23c748b5246a01edd9f59
Peter Vietense <peter.vietense@hotmail.de>
2023-02-19 11:24:55 +0100

    use the old nuget package for uploading coverage reports, since the new package does not recognize our coverage reports

--- cake/Tasks/UploadCodecovReport.cs
+++ cake/Tasks/UploadCodecovReport.cs
@@ -1,6 +1,9 @@
 using Cake.Codecov;
 using Cake.Common;
+using Cake.Common.Build;
+using Cake.Common.Diagnostics;
 using Cake.Common.IO;
+using Cake.Core;
 using Cake.Frosting;
 
 namespace Build
@@ -10,7 +13,17 @@ namespace Build
     {
         public override void Run(BuildContext context)
         {
-            context.Codecov(new[] { context.CoberturaResultFile.FullPath }, context.EnvironmentVariable("CODECOV_TOKEN"));
+            var settings = new CodecovSettings()
+            {
+                Required = true,
+                Verbose = true,
+                WorkingDirectory = context.CoberturaResultsPath,
+                DisableNetwork = true,
+                Files = new[] { context.CoberturaResultFile.FullPath },
+                Token = context.EnvironmentVariable("CODECOV_TOKEN"),
+            };
+
+            context.Codecov(settings);
         }
 
         public override bool ShouldRun(BuildContext context)
