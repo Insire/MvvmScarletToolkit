3f6d7caec029897be2b23c2902b12dd23218e588
Peter Vietense <peter.vietense@hotmail.de>
2023-08-10 19:10:25 +0200

    misc fix in sample iOS project

--- src/MvvmScarletToolkit.Xamarin.Forms.Samples.iOS/Main.cs
+++ src/MvvmScarletToolkit.Xamarin.Forms.Samples.iOS/Main.cs
@@ -1,4 +1,4 @@
-﻿using UIKit;
+using UIKit;
 
 namespace MvvmScarletToolkit.Xamarin.Forms.Samples.iOS
 {
@@ -9,7 +9,7 @@ namespace MvvmScarletToolkit.Xamarin.Forms.Samples.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
