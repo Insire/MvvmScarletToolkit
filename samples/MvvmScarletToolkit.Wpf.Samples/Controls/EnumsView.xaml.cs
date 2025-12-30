4037fb5db0b334c7a5dfaf7ee946a4df408f39e9
Peter Vietense <peter.vietense@hotmail.de>
2025-12-30 09:23:40 +0100

    add back enumsview

---
+++ samples/MvvmScarletToolkit.Wpf.Samples/Controls/EnumsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace MvvmScarletToolkit.Wpf.Samples.Controls
+{
+    public partial class EnumsView : UserControl
+    {
+        public EnumsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
